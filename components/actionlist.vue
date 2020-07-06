5bde43661af755e6a8fd03ad3a850946bdf0db51
zhanglihui <zhanglihui@ele-cloud.com>
2020-07-06 11:36:33 +0800

    我的行动项里分享邀请按钮颜色修改

--- components/actionlist.vue
+++ components/actionlist.vue
@@ -52,9 +52,9 @@
 			</view>
 			<view class="flex padding justify-between" >
 				<view>
-					<button class="cu-btn bg-pink sm round" v-if="item.userId==userId "  :id="index" open-type="share">分享邀请</button>
+					<button class="cu-btn bg-light-blue sm round" v-if="item.userId==userId "  :id="index" open-type="share">分享邀请</button>
 					<button class="cu-btn bg-orange sm round  " v-else-if="item.onlooker" open-type="share">为TA打Call</button>
-					<button class="cu-btn bg-pink sm round  " v-else-if="item.userId!=userId && !item.onlooker&&item.challengeRmb<=0"  @tap="lookerClick(list,index)">围观</button>
+					<button class="cu-btn bg-green sm round  " v-else-if="item.userId!=userId && !item.onlooker&&item.challengeRmb<=0"  @tap="lookerClick(list,index)">围观</button>
 					<button class="cu-btn bg-green sm round  " v-else  @tap="lookerClick(item,index)">围观分钱</button>
 					<text class="text-gray text-df ">{{item.onlookerCount}}</text>
 				</view>
@@ -125,4 +125,5 @@
 		margin-bottom: 20rpx;
 
 	}
+	.bg-light-blue{background-color: #007AFF;}
 </style>
